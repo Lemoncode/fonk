08a1cb89d597319a50aafc48b38a510baa6938da
Braulio Diez Botella <braulio.diez@gmail.com>
2019-08-25 13:00:20 +0200

    replace map

--- src/validation-dispatcher/record-dispatcher.ts
+++ src/validation-dispatcher/record-dispatcher.ts
@@ -5,12 +5,12 @@ export const fireRecordValidations = (
   values: any,
   validations: RecordValidationFunction[]
 ): Promise<ValidationResult>[] => {
-  const validationResultsPromises: Promise<ValidationResult>[] = [];
+  let validationResultsPromises: Promise<ValidationResult>[] = [];
 
   if (areAllElementsInArrayDefined(validations)) {
-    validations.forEach(validationFn => {
-      validationResultsPromises.push(validationFn(values));
-    });
+    validationResultsPromises = validations.map(validationFn =>
+      validationFn(values)
+    );
   } else {
     console.error('One of the form record validations are not defined.');
   }
