6b1238d6e355d394ef7cb44dae6f5d69163a7a96
nasdan <idsm00012@gmail.com>
2019-08-25 23:18:54 +0200

    Add safeArrayLegth unit tests

--- src/helper.ts
+++ src/helper.ts
@@ -1,11 +1,9 @@
-export const arrayContainsEntries = <T>(collection: T[]) =>
-  Array.isArray(collection) && collection.length > 0;
-
-// TODO: add unit tests
 // TODO: Better naming for this?
-// Cover undefined and null cases, returns 0 in that case
 export const safeArrayLength = <T>(collection: T[]) =>
-  collection ? collection.length : 0;
+  Array.isArray(collection) ? collection.length : 0;
+
+export const arrayContainsEntries = <T>(collection: T[]) =>
+  safeArrayLength(collection) > 0;
 
 // https://stackoverflow.com/questions/5999998/check-if-a-variable-is-of-function-type
 export const isFunction = (v: any): v is Function => typeof v === 'function';
