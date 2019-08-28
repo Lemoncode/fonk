4484cbdca42bbff65941dc0e26392b283bbbd045
Braulio <braulio.diez@gmail.com>
2019-08-28 13:48:22 +0200

    defining tests

--- src/form-validation.spec.ts
+++ src/form-validation.spec.ts
@@ -15,4 +15,41 @@ describe('createFormValidation', () => {
     // Assert
     expect(formValidation).toBeInstanceOf(FormValidation);
   });
+
+  describe(`FieldValidations`, () => {
+    it(`should execute a field validation and fail when
+    adding a field validation in the schema on a given field
+    firing a validation for that given field that fails
+`, () => {});
+
+    it(`should execute a field validation and success when
+    adding a field validation in the schema on a given field
+    firing a validation for that given field that succeeds
+`, () => {});
+
+    it(`should execute a field validation and display custom message when
+    adding a field validation in the schema on a given field (including custom message)
+    firing a validation for that given field that fails
+`, () => {});
+
+    it(`should not execute a field validation and succeed when
+    adding a field validation in the schema on a given field
+    firing a validation for another field that fails
+`, () => {});
+
+    it(`should execute two validations for a given field
+    adding a two field succeed validations for in the schema on a given field
+    firing validation for that field
+`, () => {});
+  });
+
+  describe(`FormValidations`, () => {
+    // Test here all fields togehter
+    // Create a form validation check is fired
+    // Combine both field and form
+    // Create two form validations check both executed
+    // on succeed
+    // first fail second succeeded
+    // secondf succeeds
+  });
 });
