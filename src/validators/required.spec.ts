e4d0bb4535f3b642aba82626ca8e3f9ab39c7112
Braulio Diez Botella <braulio.diez@gmail.com>
2019-08-30 09:53:27 +0200

    unit tests completed

--- src/validators/required.spec.ts
+++ src/validators/required.spec.ts
@@ -91,5 +91,92 @@ describe(`required validator`, () => {
       expect(validationResult.succeeded).toBeTruthy();
       expect(validationResult.type).toBe(VALIDATOR_TYPE);
     });
+
+    it('should return true if typeof value is number', () => {
+      // Arrange
+      const value = 1;
+      const values = undefined;
+      const customArgs: RequiredArgs = undefined;
+
+      // Act
+      const validationResult = required(value, values, customArgs);
+
+      // Assert
+      expect(validationResult.succeeded).toBeTruthy();
+      expect(validationResult.type).toBe(VALIDATOR_TYPE);
+    });
+  });
+  describe('When validating a string value', () => {
+    it('should return false if string is empty', () => {
+      // Arrange
+      const value = '';
+      const values = undefined;
+      const customArgs: RequiredArgs = undefined;
+
+      // Act
+      const validationResult = required(value, values, customArgs);
+
+      // Assert
+      expect(validationResult.succeeded).toBeFalsy();
+      expect(validationResult.type).toBe(VALIDATOR_TYPE);
+      expect(validationResult.message).toBe(
+        'Please fill in this mandatory field.'
+      );
+    });
+    it('should return succeed if string has whitespace characters and trim option is false', () => {
+      // Arrange
+      const value = ' ';
+      const values = undefined;
+      const customArgs: RequiredArgs = { trim: false };
+
+      // Act
+      const validationResult = required(value, values, customArgs);
+
+      // Assert
+      expect(validationResult.succeeded).toBeTruthy();
+      expect(validationResult.type).toBe(VALIDATOR_TYPE);
+    });
+
+    it('should return true if string has whitespace characters and trim option is false', () => {
+      // Arrange
+      const value = ' ';
+      const values = undefined;
+      const customArgs: RequiredArgs = { trim: false };
+
+      // Act
+      const validationResult = required(value, values, customArgs);
+
+      // Assert
+      expect(validationResult.succeeded).toBeTruthy();
+      expect(validationResult.type).toBe(VALIDATOR_TYPE);
+    });
+
+    it('should return succeed if string has whitespace characters and trim option is null', () => {
+      // Arrange
+      const value = ' ';
+      const values = undefined;
+      const customArgs: RequiredArgs = { trim: null };
+
+      // Act
+      const validationResult = required(value, values, customArgs);
+
+      // Assert
+      expect(validationResult.succeeded).toBeTruthy();
+      expect(validationResult.type).toBe(VALIDATOR_TYPE);
+    });
+
+    it('should return failed if string has whitespace characters and trim option is undefined', () => {
+      // Arrange
+      const value = ' ';
+      const values = void 0;
+      const customArgs: RequiredArgs = null;
+
+      // Act
+      const validationResult = required(value, values, customArgs);
+
+      // Assert
+      expect(validationResult.succeeded).toBeFalsy();
+      expect(validationResult.type).toBe(VALIDATOR_TYPE);
+    });
   });
 });
