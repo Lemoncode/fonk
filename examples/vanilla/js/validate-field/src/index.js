73b3fb43d9f1ed3651de16fe322b310a5f59f24e
nasdan <daniel.sanchez@lemoncode.net>
2019-09-17 11:32:49 +0200

    add vanilla js example

---
+++ examples/vanilla/js/validate-field/src/index.js
@@ -0,0 +1,45 @@
+import { Validators, createFormValidation } from '@lemoncode/fonk';
+
+const validationSchema = {
+  field: {
+    user: [Validators.required.validator],
+    password: [Validators.required.validator],
+  },
+};
+
+const formValidation = createFormValidation(validationSchema);
+
+formValidation
+  .validateField('user', loginRecord.user)
+  .then(validationResult => {
+    document.getElementById('app').innerHTML = `
+    <div style="flex-grow: 1;margin-left:2rem;">
+      <h2>Validate field example</h2>
+
+    <pre>
+    import { Validators, createFormValidation } from "@lemoncode/fonk";
+
+    const validationSchema = {
+      field: {
+        user: [Validators.required.validator],
+        password: [Validators.required.validator]
+      }
+    };
+
+    const formValidation = createFormValidation(validationSchema);
+
+    // Execute form validation
+    formValidation
+      .validateField("user", loginRecord.user)
+      .then(validationResult => {
+        console.log(validationResult);
+      });
+    </pre>
+
+      <h3>Result: </h3>
+    <pre>
+    ${JSON.stringify(validationResult, null, 2)}
+    </pre>
+    </div>
+    `;
+  });
