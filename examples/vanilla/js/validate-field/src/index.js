7ba4e4a1049d9a7b4d617375b9f9913e2a79c97a
nasdan <daniel.sanchez@lemoncode.net>
2019-09-17 11:36:08 +0200

    Update js example

--- examples/vanilla/js/validate-field/src/index.js
+++ examples/vanilla/js/validate-field/src/index.js
@@ -9,37 +9,49 @@ const validationSchema = {
 
 const formValidation = createFormValidation(validationSchema);
 
+const loginRecord = {
+  user: '',
+  password: '',
+};
+
 formValidation
   .validateField('user', loginRecord.user)
   .then(validationResult => {
+    console.log(validationResult);
+
     document.getElementById('app').innerHTML = `
     <div style="flex-grow: 1;margin-left:2rem;">
       <h2>Validate field example</h2>
 
-    <pre>
-    import { Validators, createFormValidation } from "@lemoncode/fonk";
+<pre>
+import { Validators, createFormValidation } from "@lemoncode/fonk";
 
-    const validationSchema = {
-      field: {
-        user: [Validators.required.validator],
-        password: [Validators.required.validator]
-      }
-    };
+const validationSchema = {
+  field: {
+    user: [Validators.required.validator],
+    password: [Validators.required.validator]
+  }
+};
 
-    const formValidation = createFormValidation(validationSchema);
+const loginRecord = {
+  user: '',
+  password: '',
+};
 
-    // Execute form validation
-    formValidation
-      .validateField("user", loginRecord.user)
-      .then(validationResult => {
-        console.log(validationResult);
-      });
-    </pre>
+const formValidation = createFormValidation(validationSchema);
+
+// Execute form validation
+formValidation
+  .validateField("user", loginRecord.user)
+  .then(validationResult => {
+    console.log(validationResult);
+  });
+</pre>
 
       <h3>Result: </h3>
-    <pre>
-    ${JSON.stringify(validationResult, null, 2)}
-    </pre>
+<pre>
+${JSON.stringify(validationResult, null, 2)}
+</pre>
     </div>
     `;
   });
