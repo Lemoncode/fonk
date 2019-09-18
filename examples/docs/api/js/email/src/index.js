ff7bb54d06a6c6b0d9d2bbf0bc9ed9718316973e
nasdan <daniel.sanchez@lemoncode.net>
2019-09-18 13:00:32 +0200

    Add docs api email js example

---
+++ examples/docs/api/js/email/src/index.js
@@ -0,0 +1,39 @@
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
+Prism.highlightAll();
+import { getResults, formValues } from './playground';
+
+getResults().then(validationResult => {
+  document.getElementById('app').innerHTML = `
+    <div style="flex-grow: 1;margin-left:2rem;">
+      <h2>Email example</h2>
+
+<pre><code class="language-js">
+import { Validators, createFormValidation } from '@lemoncode/fonk';
+
+const validationSchema = {
+  field: {
+    email: [Validators.email.validator],
+  },
+};
+
+const formValidation = createFormValidation(validationSchema);
+
+// Update values in ./playground.js
+const formValues = ${JSON.stringify({ ...formValues }, null, 2)};
+
+// Execute form validation
+formValidation
+  .validateForm(formValues)
+  .then(validationResult => {
+    console.log(validationResult);
+  });
+</code></pre>
+
+<h3>Result: </h3>
+<pre><code class="language-js">
+${JSON.stringify(validationResult, null, 2)}
+</code></pre>
+</div>
+    `;
+});
