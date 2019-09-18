d5050c6836e114d12f79678873997830e10e0056
nasdan <daniel.sanchez@lemoncode.net>
2019-09-18 10:31:01 +0200

    Add Async validator react-final-form js example

---
+++ examples/react-final-form/js/async-validator/custom-validators/user-exists-on-github.validator.js
@@ -0,0 +1,19 @@
+export const userExistsOnGithubValidator = ({ value }) => {
+  const validationResult = {
+    type: "GITHUB_USER_EXISTS",
+    succeeded: false,
+    message: "The username exists on Github"
+  };
+
+  return fetch(`https://api.github.com/users/${value}`).then(response => {
+    // Status 404, User does not exists, so the given user is valid
+    // Status 200, meaning user exists, so the given user is not valid
+    return response.status === 404
+      ? {
+          ...validationResult,
+          succeeded: true,
+          message: ""
+        }
+      : validationResult;
+  });
+};
