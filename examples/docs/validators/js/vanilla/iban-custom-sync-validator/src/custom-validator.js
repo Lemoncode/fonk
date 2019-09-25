58bf82f62448dbb0b558101bea12eedfa4af3656
nasdan <daniel.sanchez@lemoncode.net>
2019-09-25 10:57:20 +0200

    Add iban validator vanilla js example

---
+++ examples/docs/validators/js/vanilla/iban-custom-sync-validator/src/custom-validator.js
@@ -0,0 +1,33 @@
+import { parseMessageWithCustomArgs } from '@lemoncode/fonk';
+
+const validatorType = 'MY_IBAN_VALIDATOR';
+
+let defaultMessage = 'IBAN does not belong to {{countryCode}}';
+export const setErrorMessage = message => (defaultMessage = message);
+
+const hasValidCountryCode = (value, customArgs) =>
+  value &&
+  value[0] === customArgs.countryCode[0] &&
+  value[1] === customArgs.countryCode[1];
+
+export const ibanValidator = fieldValidatorArgs => {
+  const { value, customArgs, message = defaultMessage } = fieldValidatorArgs;
+
+  // Depending in your case you may feed default values to customArgs or throw
+  // an exception or a console.log error
+  if (!customArgs.countryCode || customArgs.countryCode.length !== 2) {
+    throw `${validatorType}: error you should inform customArgs countryCode prefix (2 characters length)`;
+  }
+  const validationResult = {
+    succeeded: false,
+    type: validatorType,
+    message: parseMessageWithCustomArgs(message, customArgs),
+  };
+
+  if (hasValidCountryCode(value, customArgs)) {
+    validationResult.succeeded = true;
+    validationResult.message = '';
+  }
+
+  return validationResult;
+};
---
+++ examples/docs/validators/js/vanilla/iban-custom-sync-validator/src/playground.js
@@ -0,0 +1,32 @@
+import { createFormValidation } from '@lemoncode/fonk';
+import { ibanValidator } from './custom-validator';
+
+const validationSchema = {
+  field: {
+    account: [
+      {
+        validator: ibanValidator,
+        customArgs: {
+          countryCode: 'ES',
+        },
+      },
+    ],
+  },
+};
+
+const formValidation = createFormValidation(validationSchema);
+
+export const failedLoginRecord = {
+  account: '',
+};
+
+export const succeededLoginRecord = {
+  account: 'ES0051354874545454546462',
+};
+
+export const getResults = () => {
+  return Promise.all([
+    formValidation.validateField('account', failedLoginRecord.account),
+    formValidation.validateField('account', succeededLoginRecord.account),
+  ]);
+};
