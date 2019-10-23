f754614b5053289c4ee293c2322171760dc354a6
Braulio Diez Botella <braulio.diez@gmail.com>
2019-10-23 17:14:55 +0200

    field example crashing refactor needed

---
+++ examples/formik/js/fields/index.js
@@ -0,0 +1,38 @@
+// Render Prop
+import React from 'react';
+import { render } from 'react-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { formValidation } from './validate';
+
+// Watch out !! Issue Here ErrorMessage expects
+// field name, not valid to pass _password.message_
+// and error field if not plain text bad crash
+const App = () => (
+  <div>
+    <h1>Any place in your app!</h1>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={values => formValidation.validateForm(values)}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email.message" component="div" />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
