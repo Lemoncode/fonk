2b2dd12ff06e96e765f287fad58ea4c345db1bf1
Braulio <braulio.diez@gmail.com>
2019-10-24 08:56:54 +0200

    field level

---
+++ examples/formik/js/field-level-validation/index.js
@@ -0,0 +1,52 @@
+// Helper styles for demo
+import './helper.css';
+
+// Render Prop
+import React from 'react';
+import { render } from 'react-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { formValidation } from './validate';
+
+// Watch out !! Issue Here ErrorMessage expects
+// field name, not valid to pass _password.message_
+// and error field if not plain text bad crash
+const App = () => (
+  <div>
+    <h1>Any place in your app!</h1>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting, values }) => (
+        <Form>
+          <Field
+            type="email"
+            name="email"
+            validate={value =>
+              formValidation.validateField('email', value, values)
+            }
+          />
+          <ErrorMessage name="email" component="div" />
+          <Field
+            type="password"
+            name="password"
+            validate={value =>
+              formValidation.validateField('password', value, values)
+            }
+          />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
