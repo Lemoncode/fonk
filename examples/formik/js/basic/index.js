6a44e2e9e80e00f367659c68bc97a85ba2cf0895
Braulio <braulio.diez@gmail.com>
2019-10-25 13:43:16 +0200

    moving h1

--- examples/formik/js/basic/index.js
+++ examples/formik/js/basic/index.js
@@ -18,66 +18,66 @@ const App = () => (
         Fonk-Formik
       </a>{' '}
       Demo
-      <Formik
-        initialValues={{ email: '' }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 500);
-        }}
-        validate={values => formValidation.validateForm(values)}
-      >
-        {props => {
-          const {
-            values,
-            touched,
-            errors,
-            dirty,
-            isSubmitting,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            handleReset,
-          } = props;
-          return (
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="email" style={{ display: 'block' }}>
-                Email
-              </label>
-              <input
-                id="email"
-                placeholder="Enter your email"
-                type="text"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className={
-                  errors.email && touched.email
-                    ? 'text-input error'
-                    : 'text-input'
-                }
-              />
-              {errors.email && touched.email && (
-                <div className="input-feedback">{errors.email}</div>
-              )}
-              <button
-                type="button"
-                className="outline"
-                onClick={handleReset}
-                disabled={!dirty || isSubmitting}
-              >
-                Reset
-              </button>
-
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </form>
-          );
-        }}
-      </Formik>
     </h1>
+    <Formik
+      initialValues={{ email: '' }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 500);
+      }}
+      validate={values => formValidation.validateForm(values)}
+    >
+      {props => {
+        const {
+          values,
+          touched,
+          errors,
+          dirty,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          handleReset,
+        } = props;
+        return (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email" style={{ display: 'block' }}>
+              Email
+            </label>
+            <input
+              id="email"
+              placeholder="Enter your email"
+              type="text"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={
+                errors.email && touched.email
+                  ? 'text-input error'
+                  : 'text-input'
+              }
+            />
+            {errors.email && touched.email && (
+              <div className="input-feedback">{errors.email}</div>
+            )}
+            <button
+              type="button"
+              className="outline"
+              onClick={handleReset}
+              disabled={!dirty || isSubmitting}
+            >
+              Reset
+            </button>
+
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        );
+      }}
+    </Formik>
   </div>
 );
 
