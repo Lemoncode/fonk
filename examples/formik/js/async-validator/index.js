518ae050943b9558d87fdefa0eea6eef1a41cbf6
Braulio <braulio.diez@gmail.com>
2019-10-25 13:53:09 +0200

    asycn

--- examples/formik/js/async-validator/index.js
+++ examples/formik/js/async-validator/index.js
@@ -18,66 +18,69 @@ const App = () => (
         Fonk-Formik
       </a>{' '}
       Demo
-      <Formik
-        initialValues={{ user: '' }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 500);
-        }}
-        validate={values => formValidation.validateForm(values)}
-      >
-        {props => {
-          const {
-            values,
-            touched,
-            errors,
-            dirty,
-            isSubmitting,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            handleReset,
-          } = props;
-          return (
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="user" style={{ display: 'block' }}>
-                User
-              </label>
-              <input
-                id="user"
-                placeholder="Enter your user id (must be a brand new Github user)"
-                type="text"
-                value={values.user}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className={
-                  errors.user && touched.user
-                    ? 'text-input error'
-                    : 'text-input'
-                }
-              />
-              {errors.user && touched.user && (
-                <div className="input-feedback">{errors.user}</div>
-              )}
-              <button
-                type="button"
-                className="outline"
-                onClick={handleReset}
-                disabled={!dirty || isSubmitting}
-              >
-                Reset
-              </button>
-
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </form>
-          );
-        }}
-      </Formik>
     </h1>
+    <span>
+      Triggering and async validation: User is valid if it does NOT exists as a
+      Github user (hits Github API). Initially you will have to loose focus on
+      the component to see the validation result.
+    </span>
+    <Formik
+      initialValues={{ user: '' }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 500);
+      }}
+      validate={values => formValidation.validateForm(values)}
+    >
+      {props => {
+        const {
+          values,
+          touched,
+          errors,
+          dirty,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          handleReset,
+        } = props;
+        return (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="user" style={{ display: 'block' }}>
+              User
+            </label>
+            <input
+              id="user"
+              placeholder="Enter your user id (must be a brand new Github user)"
+              type="text"
+              value={values.user}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={
+                errors.user && touched.user ? 'text-input error' : 'text-input'
+              }
+            />
+            {errors.user && touched.user && (
+              <div className="input-feedback">{errors.user}</div>
+            )}
+            <button
+              type="button"
+              className="outline"
+              onClick={handleReset}
+              disabled={!dirty || isSubmitting}
+            >
+              Reset
+            </button>
+
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        );
+      }}
+    </Formik>
   </div>
 );
 
