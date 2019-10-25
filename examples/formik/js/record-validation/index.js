3ee8df03b32944f8766e01bdbb8a1bc2e824d7c5
Braulio <braulio.diez@gmail.com>
2019-10-25 11:27:34 +0200

    update

--- examples/formik/js/record-validation/index.js
+++ examples/formik/js/record-validation/index.js
@@ -11,12 +11,15 @@ import { formValidation } from './form-validation';
 const App = () => (
   <div>
     <h1>Creating Record Validations</h1>
-    <h4>
-      If price minus discount and is not prime it will show an error: 'Subscribe
-      to prime service or total must be greater than 20USD'
-    </h4>
+    <span>
+      Display record validation error message if user has not prime services
+      contracted or total is lower than 20USD: 'Subscribe to prime service or
+      total must be greater than 20USD'
+    </span>
+    <span>Code: Check form-validate.js</span>
+
     <Formik
-      initialValues={{ product: '', discount: 0, price: 0, isPrime: false }}
+      initialValues={{ product: '', discount: '', price: '', isPrime: false }}
       validate={values => formValidation.validateForm(values)}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
