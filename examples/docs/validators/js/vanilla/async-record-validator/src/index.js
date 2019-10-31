92c569992f9b161377649512c1c814a5b36f1640
nasdan <daniel.sanchez@lemoncode.net>
2019-10-31 16:44:11 +0100

    Refactor async-record-validator example

--- examples/docs/validators/js/vanilla/async-record-validator/src/index.js
+++ examples/docs/validators/js/vanilla/async-record-validator/src/index.js
@@ -1,12 +1,38 @@
-import { executeProcess } from './playground';
-const app = document.getElementById('app');
+import './styles.scss';
+import { formValidation } from './form-validation';
+import { resolveProcessFromCache } from './api';
+import { renderProcess, updateContent } from './helpers';
 
-app.innerHTML = `
-<div style="flex-grow: 1;margin-left:2rem;">
-  <button id="nextProcessButton">Execute next process</button>
-  <ul id="processList">
-  </ul>
-</div>
-`;
+const createEmptyValues = () => [];
 
-document.getElementById('nextProcessButton').onclick = executeProcess;
+let processList = createEmptyValues();
+
+const createNextProcess = index => {
+  const cachedResult = resolveProcessFromCache();
+  const process = {
+    name: `Process ${index}`,
+    cachedResult,
+  };
+  processList = [...processList, process];
+
+  return process;
+};
+
+document.getElementById('next-process-button').onclick = () => {
+  const index = processList.length;
+  const process = createNextProcess(index);
+  renderProcess(process, index);
+
+  formValidation.validateRecord(process).then(validationResult => {
+    if (!validationResult.succeeded) {
+      const message = validationResult.recordErrors.process.message;
+      updateContent(message, index);
+    }
+  });
+};
+
+const resetButton = document.getElementById('reset-button');
+resetButton.onclick = () => {
+  processList = createEmptyValues();
+  document.getElementById('processList').innerHTML = '';
+};
