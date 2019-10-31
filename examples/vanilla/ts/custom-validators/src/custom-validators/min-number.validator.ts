d08145e9ce1b16d8c9c90aa80ac5810c7a145e24
nasdan <daniel.sanchez@lemoncode.net>
2019-10-31 12:26:19 +0100

    Refactor ts custom-validators example

--- examples/vanilla/ts/custom-validators/src/custom-validators/min-number.validator.ts
+++ examples/vanilla/ts/custom-validators/src/custom-validators/min-number.validator.ts
@@ -1,14 +1,5 @@
 import { FieldValidationFunctionSync } from '@lemoncode/fonk';
 
-export const isNumberValidator: FieldValidationFunctionSync = ({ value }) => {
-  const succeeded = !isNaN(value);
-  return {
-    succeeded,
-    message: succeeded ? '' : 'Must be a number',
-    type: 'IS_NUMBER',
-  };
-};
-
 interface MinNumberArgs {
   min: number;
 }
---
+++ examples/vanilla/ts/custom-validators/src/form-validation.ts
@@ -0,0 +1,36 @@
+import {
+  Validators,
+  ValidationSchema,
+  createFormValidation,
+} from '@lemoncode/fonk';
+import { isNumberValidator, minNumberValidator } from './custom-validators';
+
+const validationSchema: ValidationSchema = {
+  field: {
+    firstName: [
+      {
+        validator: Validators.required.validator,
+        message: 'Required',
+      },
+    ],
+    lastName: [
+      {
+        validator: Validators.required.validator,
+        message: 'Required',
+      },
+    ],
+    age: [
+      {
+        validator: Validators.required.validator,
+        message: 'Required',
+      },
+      isNumberValidator,
+      {
+        validator: minNumberValidator,
+        customArgs: { min: 18 },
+      },
+    ],
+  },
+};
+
+export const formValidation = createFormValidation(validationSchema);
