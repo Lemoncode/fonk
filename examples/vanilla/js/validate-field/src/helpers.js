496ddae7c4f9b2cc576014cb81f85fde0edfe773
nasdan <daniel.sanchez@lemoncode.net>
2019-10-31 10:53:31 +0100

    Update validate-field example

---
+++ examples/vanilla/js/validate-field/src/helpers.js
@@ -0,0 +1,29 @@
+export const setErrorsByIds = ids => newErrors => {
+  ids.forEach(id => {
+    const element = document.getElementById(`${id}-error`);
+    element.textContent = newErrors[id].message;
+  });
+};
+
+export const setValuesByIds = ids => newValues => {
+  ids.forEach(id => {
+    const element = document.getElementById(id);
+    element.value = newValues[id];
+  });
+  const result = document.getElementById('result');
+  result.textContent = JSON.stringify(newValues, null, 2);
+};
+
+export const onValidateForm = (id, callback) => {
+  const element = document.getElementById(id);
+  element.onsubmit = e => {
+    e.preventDefault();
+    callback();
+  };
+};
+
+export const onValidateField = (id, callback) => {
+  const element = document.getElementById(id);
+  element.oninput = event => callback(event);
+  element.onblur = event => callback(event);
+};
--- examples/vanilla/js/validate-field/src/index.js
+++ examples/vanilla/js/validate-field/src/index.js
@@ -1,72 +1,79 @@
-import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css';
-import { getResults, failedLoginRecord, succeededLoginRecord } from './playground';
+import './styles.scss';
+import { createDefaultValidationResult } from '@lemoncode/fonk';
+import { formValidation } from './form-validation';
+import {
+  setErrorsByIds,
+  setValuesByIds,
+  onValidateField,
+  onValidateForm,
+} from './helpers';
 
-getResults().then(([failedResult, succeededResult]) => {
-  setTimeout(() => Prism.highlightAll(), 0);
-  document.getElementById('app').innerHTML = `
-    <div style="flex-grow: 1;margin-left:2rem;">
-      <h2>Example with failed result:</h2>
-
-<pre><code class="language-js">
-import { Validators, createFormValidation } from '@lemoncode/fonk';
-
-const validationSchema = {
-  field: {
-    user: [Validators.required.validator],
-    password: [Validators.required.validator],
-  },
-};
-
-const formValidation = createFormValidation(validationSchema);
-
-// Update values in ./playground.js
-const failedLoginRecord = ${JSON.stringify({ ...failedLoginRecord }, null, 2)};
-
-// Execute form validation
-formValidation
-  .validateField("user", loginRecord.user)
-  .then(validationResult => {
-    console.log(validationResult);
-  });
-</code></pre>
-
-<h3>Result: </h3>
-<pre><code class="language-js">
-${JSON.stringify(failedResult, null, 2)}
-</code></pre>
-</div>
-
-<div style="flex-grow: 1;margin-left:2rem;">
-<h2>Example with succeeded result:</h2>
-
-<pre><code class="language-js">
-import { Validators, createFormValidation } from '@lemoncode/fonk';
-
-const validationSchema = {
-  field: {
-    user: [Validators.required.validator],
-    password: [Validators.required.validator],
-  },
-};
-
-const formValidation = createFormValidation(validationSchema);
-
-// Update values in ./playground.js
-const succeededLoginRecord = ${JSON.stringify({ ...succeededLoginRecord }, null, 2)};
-
-// Execute form validation
-formValidation
-  .validateField("user", loginRecord.user)
-  .then(validationResult => {
-    console.log(validationResult);
-  });
-</code></pre>
-
-<h3>Result: </h3>
-<pre><code class="language-js">
-${JSON.stringify(succeededResult, null, 2)}
-</code></pre>
-</div>
-    `;
+const fieldIds = ['firstName', 'lastName', 'age'];
+const createEmptyValues = () => ({
+  firstName: '',
+  lastName: '',
+  age: '',
 });
+
+let values = createEmptyValues();
+
+const setValues = newValues => {
+  values = { ...newValues };
+  const set = setValuesByIds(fieldIds);
+  set(values);
+};
+
+const createEmptyErrors = () => ({
+  firstName: createDefaultValidationResult(),
+  lastName: createDefaultValidationResult(),
+  age: createDefaultValidationResult(),
+});
+
+let errors = createEmptyErrors();
+const setErrors = newErrors => {
+  errors = { ...newErrors };
+  const set = setErrorsByIds(fieldIds);
+  set(errors);
+};
+
+onValidateForm('form', () => {
+  formValidation.validateForm(values).then(validationResult => {
+    setErrors(validationResult.fieldErrors);
+    if (validationResult.succeeded) {
+      window.alert(JSON.stringify(values, null, 2));
+    }
+  });
+});
+
+onValidateField('firstName', event => {
+  const value = event.target.value;
+  setValues({ ...values, firstName: value });
+
+  formValidation.validateField('firstName', value).then(validationResult => {
+    setErrors({ ...errors, firstName: validationResult });
+  });
+});
+
+onValidateField('lastName', event => {
+  const value = event.target.value;
+  setValues({ ...values, lastName: value });
+
+  formValidation.validateField('lastName', value).then(validationResult => {
+    setErrors({ ...errors, lastName: validationResult });
+  });
+});
+
+onValidateField('age', event => {
+  const value = event.target.value;
+  setValues({ ...values, age: value });
+
+  formValidation.validateField('age', value).then(validationResult => {
+    setErrors({ ...errors, age: validationResult });
+  });
+});
+
+const resetButton = document.getElementById('reset-button');
+resetButton.onclick = () => {
+  setValues(createEmptyValues());
+  setErrors(createEmptyErrors());
+};
