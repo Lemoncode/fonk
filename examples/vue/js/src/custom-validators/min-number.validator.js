c9b17c664123e1fdc858648f6f0a05bc59b0eada
Sara Lissette <lissette.ibnz@gmail.com>
2019-11-12 17:43:33 +0000

    Remove old version

--- examples/vue/js/src/custom-validators/min-number.validator.js
+++
@@ -1,8 +0,0 @@
-export const minNumberValidator = ({ value, customArgs }) => {
-  const succeeded = isNaN(value) || value >= customArgs.min;
-  return {
-    succeeded,
-    message: succeeded ? '' : `Should be greater than ${customArgs.min}`,
-    type: 'MIN_NUMBER',
-  };
-};
