7996c1de51e41f2846565beb86435d744054f4f2
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-07-29 08:52:03 +0200

    add multiple user creation ts example

---
+++ examples/vanilla/ts/multiple-user-creation-array-validator/src/helpers.ts
@@ -0,0 +1,86 @@
+export const setErrorsByIds = ids => newErrors => {
+  ids.forEach(id => {
+    const element = document.getElementById(`${id}-error`);
+    element.textContent = newErrors[id].message;
+  });
+};
+
+export const onValidateForm = (id, callback) => {
+  const element = document.getElementById(id);
+  element.onsubmit = e => {
+    e.preventDefault();
+    callback();
+  };
+};
+
+export const onValidateField = (id, callback) => {
+  const element = document.getElementById(id);
+  element.oninput = event => callback(event);
+  element.onblur = event => callback(event);
+};
+
+export const addUser = index => {
+  const userListElement = document.getElementById('users');
+  const userElement = `
+    <button id="users[${index}]-remove-button" type="button">
+      -
+    </button>
+    <div class="input">
+      <input id="users[${index}].name" type="text" placeholder="Name" />
+      <span class="input-feedback" id="users[${index}].name-error"></span>
+    </div>
+    <div class="input">
+      <input id="users[${index}].email" type="text" placeholder="Email" />
+      <span class="input-feedback" id="users[${index}].email-error"></span>
+    </div>
+    <div class="input">
+      <input id="users[${index}].repeatEmail" type="text" placeholder="Repeat Email" />
+      <span class="input-feedback" id="users[${index}].repeatEmail-error"></span>
+    </div>
+`;
+  const div = document.createElement('div');
+  div.id = `users[${index}]`;
+  div.className = 'row';
+  div.innerHTML = userElement;
+
+  userListElement.appendChild(div);
+};
+
+export const removeUser = (users, index, onAddHandlers) => {
+  const userElement = document.getElementById(`users[${index}]`);
+  userElement.remove();
+
+  users.forEach((_, i) => {
+    if (i >= index) {
+      const oldIndex = i + 1;
+      const container = document.getElementById(`users[${oldIndex}]`);
+      container.id = `users[${i}]`;
+      const removeButton = document.getElementById(
+        `users[${oldIndex}]-remove-button`
+      );
+      removeButton.id = `users[${i}]-remove-button`;
+      const name = document.getElementById(`users[${oldIndex}].name`);
+      name.id = `users[${i}].name`;
+      const nameError = document.getElementById(
+        `users[${oldIndex}].name-error`
+      );
+      nameError.id = `users[${i}].name-error`;
+      const email = document.getElementById(`users[${oldIndex}].email`);
+      email.id = `users[${i}].email`;
+      const emailError = document.getElementById(
+        `users[${oldIndex}].email-error`
+      );
+      emailError.id = `users[${i}].email-error`;
+      const repeatEmail = document.getElementById(
+        `users[${oldIndex}].repeatEmail`
+      );
+      repeatEmail.id = `users[${i}].repeatEmail`;
+      const repeatEmailError = document.getElementById(
+        `users[${oldIndex}].repeatEmail-error`
+      );
+      repeatEmailError.id = `users[${i}].repeatEmail-error`;
+
+      onAddHandlers(i);
+    }
+  });
+};
---
+++ examples/vanilla/ts/multiple-user-creation-array-validator/src/index.ts
@@ -0,0 +1,92 @@
+import './styles.scss';
+import { formValidation } from './form-validation';
+import {
+  setErrorsByIds,
+  onValidateField,
+  onValidateForm,
+  addUser,
+  removeUser,
+} from './helpers';
+
+const createEmptyValues = () => ({
+  users: [],
+});
+const createEmptyUser = () => ({
+  name: '',
+  email: '',
+  repeatEmail: '',
+});
+
+let values = createEmptyValues();
+
+const setValues = (newValues, index) => {
+  values = { ...newValues };
+  const nameElement: any = document.getElementById(`users[${index}].name`);
+  nameElement.value = values.users[index].name;
+  const emailElement: any = document.getElementById(`users[${index}].email`);
+  emailElement.value = values.users[index].email;
+  const repeatEmailElement: any = document.getElementById(
+    `users[${index}].repeatEmail`
+  );
+  repeatEmailElement.value = values.users[index].repeatEmail;
+};
+
+const handleValidateField = (index, fieldName) => {
+  onValidateField(`users[${index}].${fieldName}`, event => {
+    const value = event.target.value;
+    const user = { ...values.users[index], [fieldName]: value };
+    const users = values.users.map((p, i) => (i === index ? user : p));
+    setValues({ ...values, users }, index);
+
+    formValidation
+      .validateField(`users`, values.users)
+      .then(validationResult => {
+        setErrors({
+          ...errors,
+          [`users[${index}].${fieldName}`]: validationResult[
+            `users[${index}].${fieldName}`
+          ],
+        });
+      });
+  });
+};
+
+const onAddHandlers = index => {
+  const removeButton = document.getElementById(`users[${index}]-remove-button`);
+  removeButton.onclick = () => {
+    const newUsers = [...values.users];
+    newUsers.splice(index, 1);
+    values = { users: newUsers };
+    removeUser(newUsers, index, onAddHandlers);
+    handleValidateForm();
+  };
+
+  Object.keys(createEmptyUser()).forEach(field => {
+    handleValidateField(index, field);
+  });
+};
+
+const addButton = document.getElementById('add-button');
+addButton.onclick = () => {
+  const index = values.users.length;
+  addUser(index);
+  const newUser = createEmptyUser();
+  values = { users: [...values.users, newUser] };
+  onAddHandlers(index);
+};
+
+let errors = {};
+const setErrors = newErrors => {
+  errors = { ...newErrors };
+  const set = setErrorsByIds(Object.keys(errors));
+  set(errors);
+  const errorElement = document.getElementById('errors');
+  errorElement.textContent = JSON.stringify(errors, null, 2);
+};
+
+const handleValidateForm = () => {
+  formValidation.validateForm(values).then(validationResult => {
+    setErrors(validationResult.fieldErrors);
+  });
+};
+onValidateForm('form', handleValidateForm);
